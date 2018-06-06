8f9bb6b47dee66ce59a5042ccfeec4926ef69fc3
DomiDre <homiedomi@gmail.com>
2018-06-06 11:36:54 +0200

    Clear allPlots

---
+++ scripts/allPlots.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -i
+# source ~/.bashrc
+
+python -u '/home/dominique/PhD/Reisen/2018-03 Juelich PPMS/Data/DD205_8/test.py'
+python -u '/home/dominique/PhD/Experimente/Probenspezifisch/Nanoparticles/DD67/SAS/simultaneousFit.py'
