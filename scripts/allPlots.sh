6136034b4d8cdc126e3a896d4c3cb81cb000cc06
DomiDre <homiedomi@gmail.com>
2018-07-07 18:08:58 +0200

    Update tem plots

--- scripts/allPlots.sh
+++ scripts/allPlots.sh
@@ -1,3 +1,5 @@
 #!/bin/bash -i
 # source ~/.bashrc
 
+python -u '/home/dominique/PhD/Experimente/Probenspezifisch/Nanoparticles/PMK18/TEM/THESIS_tem.py'
+python -u '/home/dominique/PhD/Experimente/Probenspezifisch/Nanoparticles/KWi338/TEM/THESIS_tem.py'
