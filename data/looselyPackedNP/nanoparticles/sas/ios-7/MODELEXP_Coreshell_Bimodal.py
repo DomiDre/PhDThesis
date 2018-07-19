4c216c50cd075c776f988dfbdb544fa73039ac1c
DomiDre <homiedomi@gmail.com>
2018-07-19 19:30:16 +0200

    Update SAS of IOS-7 and IOS-11. Include OA model in IOS-7

--- data/looselyPackedNP/nanoparticles/sas/ios-7/MODELEXP_Coreshell_Bimodal.py
+++ data/looselyPackedNP/nanoparticles/sas/ios-7/MODELEXP_Coreshell_Bimodal.py
@@ -6,155 +6,13 @@
 #Preparing Script for Experiment: MODELEXP
 from modelexp import App
 from modelexp.experiments.sas import SimultaneousSaxsSans
-from modelexp.models.sas import SphereCS, InstrumentalResolution
+from modelexp.models.sas import SphereCSBimodalOA, InstrumentalResolution
 from modelexp.data import XyeData
 from modelexp.fit import LevenbergMarquardt
 
 from thesis_utils.materials import sld_xray_GALAXI, sld_neutrons_5A
 from fortSAS import sphere_cs
 import numpy as np
-
-class SphereCS_Bimodal(SphereCS):
-  def initParameters(self):
-    self.params.add('r1', 100)
-    self.params.add('r2', 70)
-    self.params.add('d', 20)
-    self.params.add('sldCore', 40e-6)
-    self.params.add('sldShell', 30e-6)
-    self.params.add('sldSolvent', 10e-6)
-    self.params.add('sigR1', 0.05)
-    self.params.add('sigR2', 0.05)
-    self.params.add('sigD', 0)
-    self.params.add('i0', 1)
-    self.params.add('fraction', 0.3)
-    self.params.add('bg', 1e-6)
-
-  def initMagneticParameters(self):
-    self.params.add('dDead', 5, min=0)
-    self.params.add('magSldCore', 1e-6)
-    self.params.add('magSldShell', 0, vary=False)
-    self.params.add('magSldSolvent', 0, vary=False)
-
-    self.addConstantParam('magSldShell')
-    self.addConstantParam('magSldSolvent')
-
-
-
-  def calcModel(self):
-    self.I = self.params['i0'] * (
-      (1-self.params['fraction']) * sphere_cs.formfactor(
-        self.q,
-        self.params['r1'],
-        self.params['d'],
-        self.params['sldCore'],
-        self.params['sldShell'],
-        self.params['sldSolvent'],
-        self.params['sigR1'],
-        self.params['sigD']
-    ) + self.params['fraction'] * sphere_cs.formfactor(
-        self.q,
-        self.params['r2'],
-        self.params['d'],
-        self.params['sldCore'],
-        self.params['sldShell'],
-        self.params['sldSolvent'],
-        self.params['sigR2'],
-        self.params['sigD']
-    )) + self.params['bg']
-
-    r1, sld1 = sphere_cs.sld(
-      self.params['r1'],
-      self.params['d'],
-      self.params['sldCore'],
-      self.params['sldShell'],
-      self.params['sldSolvent']
-    )
-
-    r2, sld2 = sphere_cs.sld(
-      self.params['r2'],
-      self.params['d'],
-      self.params['sldCore'],
-      self.params['sldShell'],
-      self.params['sldSolvent']
-    )
-    self.r = np.concatenate([r1, r1[::-1], r2])
-    self.sld = np.concatenate([sld1, sld1[::-1], sld2])
-
-  def calcMagneticModel(self):
-    self.I = self.params['i0'] * (
-      (1-self.params['fraction']) * sphere_cs.magnetic_formfactor(
-        self.q,
-        self.params['r1'],
-        self.params['d'],
-        self.params['sldCore'],
-        self.params['sldShell'],
-        self.params['sldSolvent'],
-        self.params['sigR1'],
-        self.params['sigD'],
-        self.params['dDead'],
-        self.params['magSldCore'],
-        self.params['magSldShell'],
-        self.params['magSldSolvent'],
-        self.params['xi'],
-        self.params['sin2alpha'],
-        self.params['polarization'],
-    ) + self.params['fraction'] * sphere_cs.magnetic_formfactor(
-        self.q,
-        self.params['r2'],
-        self.params['d'],
-        self.params['sldCore'],
-        self.params['sldShell'],
-        self.params['sldSolvent'],
-        self.params['sigR2'],
-        self.params['sigD'],
-        self.params['dDead'],
-        self.params['magSldCore'],
-        self.params['magSldShell'],
-        self.params['magSldSolvent'],
-        self.params['xi'],
-        self.params['sin2alpha'],
-        self.params['polarization'],
-    )) + self.params['bg']
-
-
-    r1, sld1 = sphere_cs.sld(
-      self.params['r1'],
-      self.params['d'],
-      self.params['sldCore'],
-      self.params['sldShell'],
-      self.params['sldSolvent']
-    )
-
-    r2, sld2 = sphere_cs.sld(
-      self.params['r2'],
-      self.params['d'],
-      self.params['sldCore'],
-      self.params['sldShell'],
-      self.params['sldSolvent']
-    )
-    self.r = np.concatenate([r1, r1[::-1], r2])
-    self.sld = np.concatenate([sld1, sld1[::-1], sld2])
-
-    rMag1, sldMag1 = sphere_cs.sld(
-      self.params['r1']-self.params['dDead'],
-      self.params['d'],
-      self.params['magSldCore'],
-      self.params['magSldShell'],
-      self.params['magSldSolvent']
-    )
-
-    rMag2, sldMag2 = sphere_cs.sld(
-      self.params['r2']-self.params['dDead'],
-      self.params['d'],
-      self.params['magSldCore'],
-      self.params['magSldShell'],
-      self.params['magSldSolvent']
-    )
-    self.rMag = np.concatenate([rMag1, rMag1[::-1], rMag2])
-    self.sldMag = np.concatenate([sldMag1, sldMag1[::-1], sldMag2])
-
-
-
 
 saxs_sldCore = sld_xray_GALAXI['Magnetite'].real
 saxs_sldShell = sld_xray_GALAXI['Oleic Acid'].real
@@ -168,34 +26,44 @@ app = App()
 app.setExperiment(SimultaneousSaxsSans)
 dataRef = app.setData(XyeData)
 
-dataRef.loadFromFile('saxs_KWi338.xye', ['saxs'])
+dataRef.loadFromFile('saxs_KWi338.xye', ['saxs'], 0.001)
 dataRef.loadFromFile('023564_N.dat', ['sans', 'sa']) #sans_KWi338_N_8m.dat
 dataRef.loadFromFile('023567_N.dat', ['sans', 'la']) # sans_KWi338_N_2m.dat
 
 dataRef.sliceDomain(1e-2, 0.5)
 dataRef.plotData()
 
-modelRef = app.setModel(SphereCS_Bimodal, InstrumentalResolution)
-modelRef.setParam("r1", 34.9007522362368,  minVal = 0, maxVal = 80, vary = True)
-modelRef.setParam("r2", 12.48,  minVal = 0, maxVal = 80, vary = True)
-modelRef.setParam("sigR1", 0.0942,  minVal = 0, maxVal = 0.2, vary = False)
-modelRef.setParam("sigR2", 0.2896,  minVal = 0, maxVal = 0.8, vary = False)
-modelRef.setParam("fraction", 0.3529,  minVal = 0.1, maxVal = 1, vary = True)
-modelRef.setParam("i0_saxs", 0.47500000000000003,  minVal = 0, maxVal = 1, vary = True)
-modelRef.setParam("bg_saxs", 0.0011400000000000002,  minVal = 0, maxVal = 0.02, vary = False)
+modelRef = app.setModel(SphereCSBimodalOA, InstrumentalResolution)
+modelRef.setParam("r1", 35.17974095275273,  minVal = 0, maxVal = 80, vary = True)
+modelRef.setParam("r2", 9.33953179608812,  minVal = 0, maxVal = 80, vary = True)
+modelRef.setParam("sigR1", 0.07549776216868476,  minVal = 0, maxVal = 0.2, vary = True)
+modelRef.setParam("sigR2", 0.6050139188259823,  minVal = 0, maxVal = 1.5, vary = True)
+modelRef.setParam("fraction", 0.5363056033284519,  minVal = 0.1, maxVal = 1, vary = True)
+modelRef.setParam("i0_saxs", 0.609330304519774,  minVal = 0, maxVal = 5, vary = True)
+modelRef.setParam("i0_saxs", 0.609330304519774,  minVal = 0, maxVal = 5, vary = True)
+modelRef.setParam("bg_saxs", 0.00134,  minVal = 0, maxVal = 0.02, vary = False)
 
-modelRef.setConstantParam("sldShell_sans", 3.3751693454419203e-06)
-modelRef.setConstantParam("i0_sans", 1.1)
-modelRef.setConstantParam("bg_sans", 0.0)
-modelRef.setConstantParam("dTheta_sa", 0.004096000000000001)
-modelRef.setConstantParam("dTheta_la", 0.006869799874829066)
-modelRef.setConstantParam("d", 18.96)
+modelRef.setParam("d", 18.896805666596023,  minVal = 0, maxVal = 30, vary = True)
+modelRef.setParam("i0_sans", 0.2724857638871925,  minVal = 0, maxVal = 5, vary = True)
+modelRef.setParam("bg_sans", 0.0,  minVal = 0, maxVal = 0.02, vary = False)
+modelRef.setParam("sldShell_sans", 1.16120612403591e-06,  minVal = 3.12e-08, maxVal = 5.664e-06, vary = True)
+
+modelRef.setParam("i0Oleic", 1.0431048323153003,  minVal = 0, maxVal = 30, vary = True)
+
+modelRef.setConstantParam("dTheta_sa", 0.0030600000000000002)
+modelRef.setConstantParam("dTheta_la", 0.0035800000000000003)
 modelRef.setConstantParam("sigD", 0)
+
 modelRef.setConstantParam("sldCore_saxs", saxs_sldCore)
 modelRef.setConstantParam("sldShell_saxs", saxs_sldShell)
 modelRef.setConstantParam("sldSolvent_saxs", saxs_sldSolvent)
+modelRef.setConstantParam('sldOleic_saxs', saxs_sldShell)
+
 modelRef.setConstantParam("sldCore_sans", sans_sldCore)
+#
 modelRef.setConstantParam("sldSolvent_sans", sans_sldSolvent)
+modelRef.setConstantParam('sldOleic_sans', sans_sldShell)
+
 modelRef.setConstantParam('wavelength', 5.9984)
 modelRef.setConstantParam('dWavelength', 0.04247)
 modelRef.updateModel()
