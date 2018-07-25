a789d0f62d02068cef4ea4fd22378533cbcd6506
DomiDre <homiedomi@gmail.com>
2018-07-25 20:08:31 +0200

    Add a bit about looselyNP SEM images evaluation

---
+++ data/looselyPackedNP/lps-11/sem/THESIS_semTop.py
@@ -0,0 +1,26 @@
+#Initialized ScriptFactory v0.2
+#Date: 2018-07-25 11:04:23.326529
+#Author(s)/Contact:
+#Dominique Dresen	 Dominique.Dresen@uni-koeln.de
+
+#Preparing Script for Experiment: THESIS
+import sys, os
+thesisimgs = os.environ['phdthesisimgs']
+cwd = sys.path[0]
+import matplotlib.pyplot as plt
+plt.style.use('phdthesis')
+
+from EM.sem_fft import SEM_FFT
+
+semFilepath = cwd+"/ES_S14_2_01.tif"
+chapter = 'looselyPackedNP'
+sample_name = 'ES_S14_TopView'
+savefile = chapter+'_SEM_'+sample_name
+
+semFFT = SEM_FFT()
+semFFT.load_tif_file(semFilepath)
+
+semFFT.pretty_plot(200, 100, 736, 624, 100)
+
+semFFT.fig.savefig(cwd+'/'+savefile, bbox_inches='tight')
+semFFT.fig.savefig(thesisimgs+'/'+savefile, bbox_inches='tight')
