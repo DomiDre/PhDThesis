86b7fb14eccd387ab2f2eea098558ee9893458e9
DomiDre <homiedomi@gmail.com>
2018-10-10 17:55:36 +0200

    Add magnetic field application remark

--- data/monolayers/preparation/dryingConditions/THESIS_sem.py
+++ data/monolayers/preparation/dryingConditions/THESIS_sem.py
@@ -13,9 +13,9 @@ plt.style.use('phdthesis')
 from EM.sem_fft import SEM_FFT
 
 
-def plot_tiffile(semFilepath, sample_name):
+def plot_tiffile(semFilepath, sample_name, label):
   chapter = 'monolayers'
-  savefile = chapter + '_SEM_' + sample_name
+  savefile = chapter + '_SEM_' + label
 
   semFFT = SEM_FFT()
   semFFT.load_tif_file(semFilepath)
@@ -23,7 +23,23 @@ def plot_tiffile(semFilepath, sample_name):
   # semFFT.show()
   semFFT.pretty_plot(200, 100, 736, 624, 100, sample_name)
   semFFT.fig.savefig(cwd + '/' + savefile, bbox_inches='tight')
-  # semFFT.fig.savefig(thesisimgs+'/'+savefile, bbox_inches='tight')
+  semFFT.fig.savefig(thesisimgs+'/'+savefile, bbox_inches='tight')
 
-plot_tiffile(cwd+"/DD202_1_05.tif", 'Before Washing')
-plot_tiffile(cwd+"/DD202_1_washed_09.tif", 'After Washing')
+def plot_tiffile_with_arrow(semFilepath, sample_name, label):
+  chapter = 'monolayers'
+  savefile = chapter + '_SEM_' + label
+
+  semFFT = SEM_FFT()
+  semFFT.load_tif_file(semFilepath)
+  # semFFT.plot_sem_image()
+  # semFFT.show()
+  semFFT.pretty_plot(200, 100, 736, 624, 100, sample_name)
+  semFFT.ax.arrow(1400, 1200, 200, 100, color='white', width=10)
+  semFFT.ax.text(1500, 1170, "$B_{dry}$", color='white')
+  semFFT.fig.savefig(cwd + '/' + savefile, bbox_inches='tight')
+  semFFT.fig.savefig(thesisimgs+'/'+savefile, bbox_inches='tight')
+
+# plot_tiffile(cwd+"/DD202_1_05.tif", 'Before Washing')
+# plot_tiffile(cwd+"/DD202_1_washed_09.tif", 'After Washing')
+plot_tiffile(cwd+"/DD224_4_01.tif", 'Without Magnetic Field', 'without_mag_field')
+plot_tiffile_with_arrow(cwd+"/DD224_5_Mag_03.tif", 'With Magnetic Field', 'with_mag_field')
